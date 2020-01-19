7b08efac96ddfecf96d9720420d3eed7b20f3c8a
Derek Zhan <derekjan1240@gmail.com>
2020-01-19 15:49:18 +0800

    add queue2stacks

---
+++ TwoStackQueue.py
@@ -0,0 +1,17 @@
+class Queue2stacks(object):
+
+    def __init__(self):
+
+        # Two stacks
+        self.instack = []
+        self.outstack = []
+    
+    def enqueue(self, item):
+        self.instack.append(item)
+
+    def dequeue(self):
+        if not self.outstack:
+            while self.instack:
+                self.outstack.append(self.instack.pop())
+        
+        return self.outstack.pop()
