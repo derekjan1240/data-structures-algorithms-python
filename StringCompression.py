8b3c27db75ed8563f5abc502be2dc49f6bc590ba
Derek Zhan <derekjan1240@gmail.com>
2020-01-19 14:32:26 +0800

    add StringCompression

---
+++ StringCompression.py
@@ -0,0 +1,29 @@
+def stringCompression(s):
+    r = ""
+    l = len(s)
+
+    if l == 0:
+       return ""
+
+    if l == 1:
+        return str + "1"
+
+    last = s[0]
+    cnt = 1
+    i = 1
+
+    while i < l:
+
+        if s[i] == s[i-1]: 
+            cnt +=1
+        else:
+            r = r + s[i-1] + str(cnt)
+            cnt =1
+        
+        i +=1
+    
+    r = r + s[i-1] + str(cnt)
+
+    return r
+
+print(stringCompression('AABCCCD'))
