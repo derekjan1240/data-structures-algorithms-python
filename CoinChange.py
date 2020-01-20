203292507bfe273a78338bab9dce8a4b39831f10
Derek Zhan <derekjan1240@gmail.com>
2020-01-20 09:23:28 +0800

    fix code

--- CoinChange.py
+++ CoinChange.py
@@ -12,7 +12,7 @@ def rec_coin(target, coins):
         for i in [c for c in coins if c <= target]:
 
             # Add a coin count + Recursive
-            num_coins = 1 + rec_coin(target-1, coins)
+            num_coins = 1 + rec_coin(target-i, coins)
 
             # Reset minimum is the new num_coins less than min_coins
             if num_coins < min_coins:
@@ -40,7 +40,7 @@ def rec_coin_dynam(target, coins, know_results):
         #For every coin value that is <= target
         for i in [c for c in coins if c <= target]:
 
-            num_coins = 1 + rec_coin_dynam(target-1, coins, know_results)
+            num_coins = 1 + rec_coin_dynam(target-i, coins, know_results)
 
             if num_coins < min_coins:
 
@@ -52,8 +52,8 @@ def rec_coin_dynam(target, coins, know_results):
     return min_coins    
 
 
-target = 15
-coins = [1, 5, 10, 25]
+target = 74
+coins = [1,5,10,25]
 known_results = [0]*(target+1)
 
-print( rec_coin_dynam(target, coins, known_results))
+print(rec_coin_dynam(target,coins,known_results))
